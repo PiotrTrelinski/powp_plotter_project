f5eb8f8e8277a16d09356ba7b0c9093915855fa0
Piotr Treliński <195078@edu.p.lodz.pl>
2017-05-04 10:03:10 +0200

    created ComplexCommand

---
+++ src/main/java/edu/iis/powp/command/ComplexCommand.java
@@ -0,0 +1,17 @@
+package edu.iis.powp.command;
+
+import java.util.ArrayList;
+
+import edu.iis.client.plottermagic.IPlotter;
+
+public class ComplexCommand extends ArrayList<PlotterCommand> implements PlotterCommand{
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void execute(IPlotter plotter) {
+		for(PlotterCommand command : this){
+			command.execute(plotter);
+		}
+	}
+}
